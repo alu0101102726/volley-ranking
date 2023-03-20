977868337b47ed47135454a4d21ae3cddcbac05a
Carlos Díaz <alu0101102726@ull.edu.es>
2023-03-20 23:22:57 +0000

    show, hide login and register

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 const loginForm = document.getElementById("main");
 const registerForm = document.getElementById("create-acct");
-const formContainer = document.getElementById("login-container-box");
+const formContainer = document.getElementsByClassName("login-container-box");
 
 registerForm.addEventListener("click", ( )=>{
     formContainer.classList.add("active");
