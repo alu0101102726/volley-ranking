545f5a046c23116e581d3d248a73283baec54cfb
Carlos Díaz <alu0101102726@ull.edu.es>
2023-03-20 23:32:39 +0000

    fixed dom access

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 const loginForm = document.querySelector("#main");
 const registerForm = document.querySelector("#create-acct");
-const formContainer = document.querySelector(",login-container-box");
+const formContainer = document.querySelector(".login-container-box");
 
 registerForm.addEventListener("click", ( )=>{
     console.log(formContainer);
