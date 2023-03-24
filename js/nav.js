e751b681b1b77fa4ff617575f93ba9201724dc13
Carlos Díaz <alu0101102726@ull.edu.es>
2023-03-24 12:09:37 +0000

    Added typewritter text

--- js/nav.js
+++ js/nav.js
@@ -6,3 +6,7 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
