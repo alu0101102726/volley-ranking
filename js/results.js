e05c8870e55d561bde1b563d7762a901f4703649
Carlos Díaz <alu0101102726@ull.edu.es>
2023-03-28 15:23:47 +0100

    Fixed ranking tier results

--- js/results.js
+++ js/results.js
@@ -8,6 +8,16 @@ const labels = ["S", "A", "B", "C", "D", "E"];
 const response = await fetch(endpoint);
 const data = await response.json();
 
+function compare( a, b ) {
+    if ( a.percentage <= b.percentage ){
+      return 1;
+    }
+    if ( a.percentage > b.percentage ){
+      return -1;
+    }
+    return 0;
+  }
+
 tiers.forEach((row, index) => {
     const label = row.querySelector('.label');
     label.style.backgroundColor = colors[index];
@@ -16,21 +26,33 @@ tiers.forEach((row, index) => {
 let totalVotes = data[0].totalVotes;
 let maxCategoryVal = parseInt((totalVotes / labels.length).toFixed(0));
 let limitLabel = [{}]
+let allVotes = []
 
-for( let i = 0; i < totalVotes; i += maxCategoryVal ) {
-    let index = i / maxCategoryVal;
-    limitLabel[index] = {
-        label: labels[labels.length - index - 1],
-        value: i
+let ranking = (data.length / labels.length).toFixed(0)
+let remaider = data.length % labels.length
+
+for( let i = 0; i < labels.length - 1; i++ ) {
+    limitLabel[i] = {
+        label: labels[labels.length - i - 1],
+        value: ranking
     }
 }
+
+limitLabel[labels.length - 1] = {
+    label: labels[0],
+    value: ranking
+}
+
+let labelIndex = limitLabel.length - 1;
+data.sort((a,b) => (a.percentage > b.percentage) ? -1 : ((b.percentage <= a.percentage) ? 1 : 0))
+
 for (const option of data) {
     let percentage = option.percentage;
     let currentVote = ((percentage * totalVotes) / 100).toFixed(0);
-    let i = limitLabel.length - 1;
-    console.log(currentVote)
+    let i = labelIndex;
+    let counter = 0;
     while( i >= 0 ) {
-        if(currentVote >= limitLabel[i].value) {
+        if(counter < limitLabel[i].value) {
             let user = option.label;
             let userInfo = userdata.find(element => element.name == user)
 
@@ -47,6 +69,10 @@ for (const option of data) {
             tier.appendChild(card);
             break;
         }
+        counter++
         i--; 
     }
+    labelIndex--;
 }
+
+console.log(allVotes)
